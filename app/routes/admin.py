108e45319eaf26ab6a844032d5e526faeafbb0a9
geek2geeks <ukpedropt@hotmail.com>
2023-09-08 14:00:20 +0100

    Refactor admin routes and templates:
    - Update amenity selection based on IDs instead of names in room_form route.
    - Save only the relative path for photos in the database.
    - Use url_for consistently in templates for better maintainability.
    - Add comments to admin.py for clarity.

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,5 +1,6 @@
-# Relative path: /c:/Users/ukped/OneDrive - RTC Education Ltd/Desktop/hotel booking system/Hotel-Booking-System/app/routes/admin.py
+# app/routes/admin.py
 
+# Importing required modules and classes.
 from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
 from app.models import Room, User, Amenity, Booking, Photo
 from functools import wraps
@@ -10,13 +11,17 @@ from werkzeug.utils import secure_filename
 from flask import jsonify
 import os
 
+# Blueprint for the admin related routes.
 admin = Blueprint('admin', __name__)
 
+# Allowed file extensions for room photos.
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+# Helper function to check if uploaded file has allowed extensions.
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Decorator to restrict access only to admin users.
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -26,6 +31,7 @@ def admin_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+# Error handlers for various exceptions that may arise.
 @admin.errorhandler(IntegrityError)
 def handle_db_error(e):
     db.session.rollback()
@@ -37,78 +43,40 @@ def handle_generic_error(e):
     flash('An unexpected error occurred. Please try again later.', 'danger')
     return redirect(url_for('admin.admin_dashboard'))
 
-class CustomError(Exception):
-    pass
-
-@admin.errorhandler(CustomError)
-def handle_custom_error(e):
-    flash(str(e), 'danger')
-    return redirect(url_for('admin.admin_dashboard'))
-
 @admin.errorhandler(404)
 def handle_not_found(e):
     flash('The resource you are looking for was not found.', 'warning')
     return redirect(url_for('admin.admin_dashboard'))
 
+# Route to bulk delete users.
 @admin.route('/bulk-delete-users', methods=['POST'])
 @login_required
 @admin_required
 def bulk_delete_users():
-    user_ids = request.form.getlist('user_ids')  # Assuming you send the user IDs as a list in the POST data
-
+    user_ids = request.form.getlist('user_ids')  
     if not user_ids:
         flash('No users selected for deletion.', 'warning')
         return redirect(url_for('admin.manage_users'))
-
-    # Deleting users based on their IDs
     User.query.filter(User.id.in_(user_ids)).delete(synchronize_session=False)
     db.session.commit()
-    
     flash(f'{len(user_ids)} users deleted successfully!', 'success')
     return redirect(url_for('admin.manage_users'))
 
-
+# Admin dashboard main route.
 @admin.route('/dashboard')
 @login_required
 @admin_required
 def admin_dashboard():
     return render_template('admin/admin_dashboard.html')
 
-@admin.route('/booking/<int:booking_id>/delete', methods=['POST'])
-@login_required
-@admin_required
-def delete_booking(booking_id):
-    booking = Booking.query.get(booking_id)
-    if not booking:
-        flash('Booking not found.', 'danger')
-        return redirect(url_for('admin.admin_dashboard'))
-    
-    room_id = booking.room_id
-    db.session.delete(booking)
-    db.session.commit()
-    flash('Booking deleted successfully!', 'success')
-    return redirect(url_for('admin.view_room_bookings', room_id=room_id))
-
-
-@admin.route('/view-room-bookings/<int:room_id>', methods=['GET'])
-@login_required
-@admin_required
-def view_room_bookings(room_id):
-    room = Room.query.get(room_id)
-    if not room:
-        flash('Room not found.', 'danger')
-        return redirect(url_for('admin.admin_dashboard'))
-    
-    bookings = Booking.query.filter_by(room_id=room_id).all()
-    return render_template('admin/view_room_bookings.html', room=room, bookings=bookings)
-
+# Route to list all rooms.
 @admin.route('/list-rooms')
 @admin_required
 def list_rooms_for_admin():
     rooms = Room.query.all()
     return render_template('admin/all_rooms.html', rooms=rooms)
 
-
+# Route to manage all users.
 @admin.route('/manage-users')
 @login_required
 @admin_required
@@ -116,6 +84,7 @@ def manage_users():
     users = User.query.all()
     return render_template('admin/manage_users.html', users=users)
 
+# Route to view a specific room.
 @admin.route('/view-room/<int:room_id>', methods=['GET'])
 @login_required
 @admin_required
@@ -124,9 +93,9 @@ def view_room(room_id):
     if not room:
         flash('Room not found.', 'danger')
         return redirect(url_for('admin.admin_dashboard'))
-    
     return render_template('admin/view_room.html', room=room)
 
+# Route to view and edit details of a specific user.
 @admin.route('/user/<int:user_id>', methods=['GET', 'POST'])
 @login_required
 @admin_required
@@ -135,16 +104,15 @@ def user_detail(user_id):
     if not user:
         flash('User not found.', 'danger')
         return redirect(url_for('admin.manage_users'))
-    
     if request.method == 'POST':
         user.email = request.form['email']
         user.is_admin = 'admin' in request.form
         db.session.commit()
         flash('User details updated!', 'success')
         return redirect(url_for('admin.user_detail', user_id=user_id))
-    
     return render_template('admin/user_detail.html', user=user)
 
+# Route to delete a specific user.
 @admin.route('/delete-user/<int:user_id>', methods=['POST'])
 @login_required
 @admin_required
@@ -153,13 +121,12 @@ def delete_user(user_id):
     if not user:
         flash('User not found.', 'danger')
         return redirect(url_for('admin.manage_users'))
-    
     db.session.delete(user)
     db.session.commit()
     flash('User deleted!', 'success')
     return redirect(url_for('admin.manage_users'))
 
-# Existing route for adding/editing room
+# Route for adding or editing a room.
 @admin.route('/room-form/<int:room_id>', methods=['GET', 'POST'])
 @admin.route('/room-form', defaults={'room_id': None}, methods=['GET', 'POST'])
 @login_required
@@ -177,16 +144,20 @@ def room_form(room_id):
         room.description = request.form['description']
         room.price = float(request.form['room_price'])
         
-        selected_amenities = request.form.getlist('amenities')
-        room.amenities = Amenity.query.filter(Amenity.name.in_(selected_amenities)).all()
+        # Updating the amenities based on their IDs
+        selected_amenities_ids = [int(id) for id in request.form.getlist('amenities')]
+        room.amenities = Amenity.query.filter(Amenity.id.in_(selected_amenities_ids)).all()
 
         if 'photos' in request.files:
             for photo in request.files.getlist('photos'):
                 if photo and allowed_file(photo.filename):
                     filename = secure_filename(photo.filename)
-                    photo_path = os.path.join(current_app.static_folder, 'room_photos', filename)
-                    photo.save(photo_path)
-                    new_photo = Photo(path=photo_path)
+                    photo_path_full = os.path.join(current_app.static_folder, 'room_photos', filename)
+                    # Save the photo to the static folder
+                    photo.save(photo_path_full)
+                    # Only save the relative path to the database
+                    relative_photo_path = os.path.join('room_photos', filename)
+                    new_photo = Photo(path=relative_photo_path)
                     room.photos.append(new_photo)
 
         db.session.commit()
@@ -195,18 +166,4 @@ def room_form(room_id):
 
     return render_template('admin/room_form.html', room=room)
 
-@admin.route('/delete-photo/<int:photo_id>', methods=['POST'])
-@login_required
-@admin_required
-def delete_photo(photo_id):
-    photo = Photo.query.get(photo_id)
-    if not photo:
-        flash('Photo not found.', 'danger')
-        return redirect(url_for('admin.admin_dashboard'))
 
-    room_id = photo.room_id
-    os.remove(photo.path)  # Delete from filesystem
-    db.session.delete(photo)  # Delete from database
-    db.session.commit()
-
-    return jsonify(status='success'), 200
